6a99aedf15fb86c7d983658c9af4aec72dccda0c
Azeem Yoonus <azeemyoonus4@gmail.com>
2021-03-29 22:09:44 +0530

    created connect to internet app till creating url

---
+++ Connect_To_Internet/ConnectToInternet/app/src/main/java/com/example/connecttointernet/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.connecttointernet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.Network;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+/* import com.example.android.datafrominternet.utilities.NetworkUtlis; */
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    TextView resutbox, viewbox;
+    EditText searchbox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        searchbox =(EditText) findViewById(R.id.search_box);
+        viewbox =(TextView) findViewById(R.id.view_box);
+        resutbox =(TextView) findViewById(R.id.result_box);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if(menuItemThatWasSelected==R.id.action_search){
+            Context context =MainActivity.this;
+            String message="Search clicked";
+            Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void makegithubquery(){
+        String githubquery = searchbox.getText().toString();
+      /*  URL githubsearchurl = NetworkUtlis.buildUrl(githubquery);
+        viewbox.setText(githubsearchurl.toString()); */
+    }
+}
